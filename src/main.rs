5bd9b5633ac9de7356ce8ed55af06da4d2f7bf09
Joshua Haring-Kaye <jharingkaye@gmail.com>
2021-09-25 13:05:39 -0400

    Created entire program to learn Rust

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number);
+
+    let mut exit = false;
+
+    while exit == false {
+        println!("Please input your guess!");
+
+        // In Rust, variables are immutable (unchangeable) by default
+        // mut keyword forces the variable to be mutable
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win");
+                exit = true;
+            }
+        }
+    }
+}
